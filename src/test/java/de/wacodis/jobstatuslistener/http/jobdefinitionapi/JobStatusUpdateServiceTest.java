67f01ca6548c8ac0ac820963917df16ba9d6dea9
Arne Vogt <arne.vogt@hs-bochum.de>
2019-12-13 13:23:28 +0100

    update unit tests

--- src/test/java/de/wacodis/jobstatuslistener/http/jobdefinitionapi/JobStatusUpdateServiceTest.java
+++ src/test/java/de/wacodis/jobstatuslistener/http/jobdefinitionapi/JobStatusUpdateServiceTest.java
@@ -5,19 +5,28 @@
  */
 package de.wacodis.jobstatuslistener.http.jobdefinitionapi;
 
+import de.wacodis.jobstatuslistener.configuration.JobDefinitionAPIConfig;
 import de.wacodis.jobstatuslistener.exception.JobStatusUpdateExeception;
-import de.wacodis.jobstatuslistener.model.WacodisJobDefinition;
 import de.wacodis.jobstatuslistener.model.WacodisJobStatus;
 import de.wacodis.jobstatuslistener.model.WacodisJobStatusUpdate;
 import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  *
  * @author Arne
  */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {JobStatusUpdateService.class, JobDefinitionAPIConfig.class})
 public class JobStatusUpdateServiceTest {
 
+    @Autowired
+    JobStatusUpdateService updateService;
+    
     public JobStatusUpdateServiceTest() {
     }
 
@@ -39,22 +48,20 @@ public class JobStatusUpdateServiceTest {
 
     @org.junit.jupiter.api.Test
     public void testUpdateStatus_NullStatus() throws Exception {
-        JobStatusUpdateService updateService = new JobStatusUpdateService();
         WacodisJobStatusUpdate newJobStatus = new WacodisJobStatusUpdate();
         newJobStatus.setWacodisJobIdentifier(UUID.fromString("61d324be-27ff-46c7-9c07-bb22ef30661b"));
         newJobStatus.setNewStatus(null);
 
-        assertThrows(JobStatusUpdateExeception.class, () -> updateService.updateStatus(newJobStatus));
+        assertThrows(JobStatusUpdateExeception.class, () -> this.updateService.updateStatus(newJobStatus));
     }
 
     @org.junit.jupiter.api.Test
     public void testUpdateStatus_NullIdentifier() throws Exception {
-        JobStatusUpdateService updateService = new JobStatusUpdateService();
         WacodisJobStatusUpdate newJobStatus = new WacodisJobStatusUpdate();
         newJobStatus.setNewStatus(WacodisJobStatus.WAITING);
         newJobStatus.setWacodisJobIdentifier(null);
 
-        assertThrows(JobStatusUpdateExeception.class, () -> updateService.updateStatus(newJobStatus));
+        assertThrows(JobStatusUpdateExeception.class, () -> this.updateService.updateStatus(newJobStatus));
     }
 
 }
